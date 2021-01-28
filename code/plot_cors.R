2ada2490805af547bb4a32c9331576e433f56cec
Michael Boerman <61358854+michaelboerman@users.noreply.github.com>
2021-01-27 19:20:37 -0500

    remove avg line (confusing to reader's eyes). placeholder for separate plot function file.

---
+++ code/plot_cors.R
@@ -0,0 +1,33 @@
+# function for plots
+
+
+### PLOT CORRELATIONS
+# function name: plot_correlations
+# params:
+#   df:           the dataframe of inputs (can be any dimension)
+#   x_series:     column name that should be on the x (default: date)
+#   y_series:     column name that should be on the y (default: correlations)
+#   title
+#   subtitle
+#   caption
+#   average line: logical if you want to include the average line
+
+plot_correlations <- function(df, x_series, y_series, title, subtitle, caption, avg_line){
+# placeholder....
+  }
+
+
+### PLOT NUMBER OBS USED
+# function name: plot_obs_used
+# params:
+#   df:           the dataframe of inputs (can be any dimension)
+#   x_series:     column name that should be on the x (default: date)
+#   y_series:     column name that should be on the y (default: count)
+#   title
+#   subtitle
+#   caption
+#   average line: logical if you want to include the average line
+
+plot_obs_used <- function(df, x_series, y_series, title, subtitle, caption, avg_line){
+  # placeholder....
+}
