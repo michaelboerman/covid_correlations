a0dcd067edd259459108c0e14b0c2d0f8d269b3d
Michael Boerman <61358854+michaelboerman@users.noreply.github.com>
2021-04-06 13:01:25 -0400

    update charts with latest data; run stylr.

--- code/plot_cors.R
+++ code/plot_cors.R
@@ -12,29 +12,30 @@
 #   filename:     name of file for the resulting plot (excluding path and extension)
 
 # TEST VALUES
-# x_series <- 
-# y_series <- 
-# title    <- 
-# subtitle <- 
-# caption  <- 
-# filename <- 
+# x_series <-
+# y_series <-
+# title    <-
+# subtitle <-
+# caption  <-
+# filename <-
 
 
 # function name: plot_correlations
-plot_correlations <- function(df, x_series, y_series, ylim, title, subtitle, caption, filename, avg_line = FALSE){
-
+plot_correlations <- function(df, x_series, y_series, ylim, title, subtitle, caption, filename, avg_line = FALSE) {
   df %>%
-    
+
     # remove last two obs, since some counties delay reporting.
     head(-2) %>%
-    
+
     # continue with plotting
     ggplot(aes(x = !!sym(x_series))) +
     geom_line(aes(y = !!sym(y_series))) +
     theme_minimal() +
     geom_hline(yintercept = 0) +
-    scale_y_continuous(expand = expansion(mult = 0), 
-                       limits = c(-ylim, ylim)) +
+    scale_y_continuous(
+      expand = expansion(mult = 0),
+      limits = c(-ylim, ylim)
+    ) +
     scale_x_date(
       expand = expansion(mult = 0),
       date_breaks = "2 months",
@@ -52,23 +53,24 @@ plot_correlations <- function(df, x_series, y_series, ylim, title, subtitle, cap
       axis.title.x = element_blank(),
       axis.line.y = element_line()
     ) +
-    ggsave(filename = here(paste0("plots/", filename, ".jpg")), 
-           width = 12, height = 6) +
-    NULL  
-  }
+    ggsave(
+      filename = here(paste0("plots/", filename, ".jpg")),
+      width = 12, height = 6
+    ) +
+    NULL
+}
 
 # -----------------------------------------------------------------------------#
 ### PLOT NUMBER OBS USED ###
 # function name: plot_obs_used
 # Same params as above
 
-plot_obs_used <- function(data, x_series, y_series, title, subtitle, caption, filename, avg_line = FALSE){
-  
+plot_obs_used <- function(data, x_series, y_series, title, subtitle, caption, filename, avg_line = FALSE) {
   data %>%
-    
+
     # remove last two obs, since not all countries have results.
     head(-2) %>%
-    
+
     # continue with plotting
     ggplot(aes(x = !!sym(x_series))) +
     geom_line(aes(y = !!sym(y_series))) +
@@ -85,7 +87,7 @@ plot_obs_used <- function(data, x_series, y_series, title, subtitle, caption, fi
     ) +
     labs(
       title = title,
-      subtitle = subtitle, 
+      subtitle = subtitle,
       caption = caption
     ) +
     ylab("Number of Observations") +
@@ -94,39 +96,42 @@ plot_obs_used <- function(data, x_series, y_series, title, subtitle, caption, fi
       axis.title.x = element_blank(),
       axis.line = element_line()
     ) +
-    ggsave(filename = paste0(here::here("plots"), "/", filename, ".jpg"), 
-           width = 12, height = 6) +
+    ggsave(
+      filename = paste0(here::here("plots"), "/", filename, ".jpg"),
+      width = 12, height = 6
+    ) +
     NULL
 }
 
 # -----------------------------------------------------------------------------#
 ### PLOT JUST ONE DATE ###
 plot_one_date <- function(df, date = "last", x_series, y_series, title, subtitle, caption, filename, avg_line = FALSE) {
-  
-  if (date == "last"){
+  if (date == "last") {
     one_date <- max(df$date) - 1
   } else if (date == "rand") {
     one_date <- sample(df$date, size = 1)
   } else if (is.Date(date)) {
     one_date <- date
   }
-  
+
   covid_one_date <-
     df %>%
     filter(date == one_date)
 
-  corrs <- cor.test(pull(covid_one_date[, x_series]), 
-                    pull(covid_one_date[, y_series]))
-  
+  corrs <- cor.test(
+    pull(covid_one_date[, x_series]),
+    pull(covid_one_date[, y_series])
+  )
+
   # set the annotation position
-  if (deparse(substitute(df)) == "county_level_data") { #us county
+  if (deparse(substitute(df)) == "county_level_data") { # us county
     annotation_x <- 0.075
     annotation_y <- 100000
   } else {
     annotation_x <- 3.0
     annotation_y <- 800000
   }
-  
+
   covid_one_date %>%
     drop_na(x_series, y_series) %>%
     ggplot(aes(x = !!sym(x_series), y = !!sym(y_series))) +
@@ -143,10 +148,12 @@ plot_one_date <- function(df, date = "last", x_series, y_series, title, subtitle
     ) +
     ylab("Confirmed Cases") +
     xlab("Population Density") +
-    annotate("text", x = annotation_x, y = annotation_y, 
-             size = 3,
-             label = paste0("95% Interval on\n Correlation: \n", round(corrs$conf.int, 2)[1], " to ", round(corrs$conf.int, 2)[2])) +
-    ggsave(paste0(here::here("plots"), "/", filename, ".jpg"), 
-           width = 12, height = 6)
+    annotate("text",
+      x = annotation_x, y = annotation_y,
+      size = 3,
+      label = paste0("95% Interval on\n Correlation: \n", round(corrs$conf.int, 2)[1], " to ", round(corrs$conf.int, 2)[2])
+    ) +
+    ggsave(paste0(here::here("plots"), "/", filename, ".jpg"),
+      width = 12, height = 6
+    )
 }
-
